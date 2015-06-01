c011d531f8c0292423bac45d4cdcefd154bc1eff
virginiacini <virginiacini@gmail.com>
2015-06-01 16:34:55 -0400

    added the first part of the content on A PROPOS page

--- cannebergesdion/pages/a_propos.php
+++ cannebergesdion/pages/a_propos.php
@@ -1,9 +1,22 @@
 <div id="a_propos" class="container-fluid">
 	<div class="row">
-	  <div class="col-xs-offset-2 col-xs-10 col-md-offset-4 col-md-4">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-4">
 	  	<hr>
 	  	<h2 class="page-title">À PROPOS</h2>
 	  	<hr>	
+	  </div>
+	</div><!-- end row -->
+	<div class="row">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-4">
+	  	<p class="apropos-par-01">
+	  		Canneberges Dion est une entreprise familiale spécialisée dans la production de canneberges. Basée à Sainte-Séraphine au Québec, elle 
+	  		favorise une agriculture locale afin de garantir la qualité et la traçabilité de sa production. 
+	  	</p>
+	  </div>
+	</div><!-- end row -->
+	<div class="row">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">
+	  	<img class="img-responsive" src="img/bg-canneberges-dion-apropos.jpg" />
 	  </div>
 	</div><!-- end row -->
 </div><!-- end a_propos -->
