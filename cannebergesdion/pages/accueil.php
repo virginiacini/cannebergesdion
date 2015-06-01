04fcefea8bf05c406cff8e4e0681a010d59dd860
virginiacini <virginiacini@gmail.com>
2015-06-01 11:43:05 -0400

    added sentence to home page

--- cannebergesdion/pages/accueil.php
+++ cannebergesdion/pages/accueil.php
@@ -1,3 +1,5 @@
 <div id="accueil" class="home-wrapper">
-	<div class="bg-home"></div><!-- end bg-home -->
+	<div class="bg-home">
+		<div class="txt-home"><h1>Un fruit, des bienfaits, notre passion.<h1></div>
+	</div><!-- end bg-home -->
 </div><!-- end home-wrapper -->
