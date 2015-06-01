4ca6f6fefa39fc61457bc1c9c93421d7c41dd102
virginiacini <virginiacini@gmail.com>
2015-06-01 15:53:01 -0400

    added titlw to A PROPOS page

--- cannebergesdion/pages/a_propos.php
+++ cannebergesdion/pages/a_propos.php
@@ -1,1 +1,9 @@
-<div id="a_propos" style="background: blue; height: 768px;"></div>
+<div id="a_propos" class="container-fluid">
+	<div class="row">
+	  <div class="col-xs-offset-2 col-xs-10 col-md-offset-4 col-md-4">
+	  	<hr>
+	  	<h2 class="page-title">À PROPOS</h2>
+	  	<hr>	
+	  </div>
+	</div><!-- end row -->
+</div><!-- end a_propos -->
