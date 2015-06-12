2820c997cc691d3ee4895b23a4f2e47dedfa7c79
virginiacini <virginiacini@gmail.com>
2015-06-12 18:14:13 -0400

    added product page and changed image at A PROPOS to background image

--- cannebergesdion/pages/produits.php
+++ cannebergesdion/pages/produits.php
@@ -1,1 +1,15 @@
-<div id="produits" style="background: yellow; height: 768px;"></div>
+<div id="produits" class="container-fluid">
+	<div class="row">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-4">
+	  	<hr>
+	  	<h2 class="page-title">NOS PRODUITS</h2>
+	  	<hr>	
+	  </div>
+	</div><!-- end row -->
+
+	<div class="row bg-produits">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-8">
+	  </div>
+	</div><!-- end row -->
+
+</div>
