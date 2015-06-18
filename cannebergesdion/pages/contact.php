05b99390f49b019572f277155b8d9051c747c2b8
virginiacini <virginiacini@gmail.com>
2015-06-18 14:56:06 -0400

    added title to contact page and changed added the name of the company to  the menu

--- cannebergesdion/pages/contact.php
+++ cannebergesdion/pages/contact.php
@@ -1,1 +1,9 @@
-<div id="contact" style="background: green; height: 768px;"></div>
+<div id="contact" class="container-fluid">
+	<div class="row">
+	  <div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-4">
+	  	<hr>
+	  	<h2 class="page-title">CONTACTEZ-NOUS</h2>
+	  	<hr>	
+	  </div>
+	</div><!-- end row -->
+</div>
--- cannebergesdion/widget_templates/menu.php
+++ cannebergesdion/widget_templates/menu.php
@@ -11,7 +11,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#accueil">Brand</a>
+						<a class="navbar-brand" href="#accueil">Canneberges Dion</a>
 					</div>
 
 					<!-- desktop menu-->
