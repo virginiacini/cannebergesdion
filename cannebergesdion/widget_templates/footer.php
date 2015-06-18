67f1c801852f376ed5a0b76c2feaf3057abbeda6
virginiacini <virginiacini@gmail.com>
2015-06-18 17:19:54 -0400

    added information to footer

--- cannebergesdion/widget_templates/footer.php
+++ cannebergesdion/widget_templates/footer.php
@@ -1,2 +1,7 @@
 <footer>
+	<div class="container-fluid">
+		<div class="row">
+			Copyright © 2015 Canneberges Dion Tout droits réservés. | <a href="http://winkstrategies.com/">Conception Web</a>
+		</div><!-- end row -->
+	</div>
 </footer>
