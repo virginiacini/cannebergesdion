8c2e75fdfea313808de210db396c2d3a613fb765
virginiacini <virginiacini@gmail.com>
2015-06-23 15:46:48 -0400

    edited contactform.php, adding working script

---
+++ cannebergesdion/contactform.php
@@ -0,0 +1,28 @@
+<?php
+
+if(empty($_POST['submit']))
+{
+    echo "Form is not submitted!";
+    exit;
+}
+if(empty($_POST["fullname"]) ||
+    empty($_POST["email"]))
+    {
+        echo "Please fill the form";
+        exit;
+    }
+    
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+mail( 'virginiacini@gmail.com' , 'New form submission' , 
+"New form submission: \n
+Name: $name \n
+Email:$email \n
+Message: $message" );
+
+header('Location: index.php#contact');
+
+
+?>
