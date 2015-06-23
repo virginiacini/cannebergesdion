13efec93105a7f99c0fefb78b2692150adf391e9
virginiacini <virginiacini@gmail.com>
2015-06-23 15:52:02 -0400

    edited contact.php, adding working script

--- cannebergesdion/pages/contact.php
+++ cannebergesdion/pages/contact.php
@@ -8,36 +8,35 @@
 	</div><!-- end row -->
 	
 	<div class="row">
-		<form method='post' action='contactform.php' name='contactform' id='contactform'>
-			<p>
-			<label for='fullname'>Your Name:</label><br/>
-			<input type='text' name='fullname' />
-			</p>
-
-			<p>
-			<label for='email' >Email Address:</label><br/>
-			<input type='text' name='email' />
-			</p>
-			<input type='submit' name='submit' value='Submit Form' />
-		</form>
-		<script>
-		document.contactform.onsubmit=function()
-		{
-			if(document.contactform.fullname.value =='')
-			{
-				alert("Please enter your name");
-				document.contactform.fullname.focus();
-				return false;
-			}
-			else if(document.contactform.email.value =='')
-			{
-				alert("Please enter your Email address");
-				document.contactform.email.focus();
-				return false;
-			}
-			return true;
-		}
-		</script>
+		<div class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-5">
+			<form method='post' action='contactform.php' name='contactform' id='contactform'>
+				<div class="form-group">
+					<input type='text' name='fullname'class="form-control" placeholder='Nom' />
+				</div>
+				<div class="form-group">
+					<input type='text' name='email' class="form-control" placeholder='Courriel'/>
+				</div>
+				<div class="form-group">
+					<textarea name='message' rows="4" class="form-control" cols="" placeholder="Message"></textarea>
+				</div>
+				<div class="form-group">
+					<input type='submit' name='submit' class="btn btn-default" value='Submit Form' />
+				</div>
+			</form>
+		</div>
+		<div class="col-xs-offset-1 col-xs-10 col-md-offset-1 col-md-4">
+			<div class="address">
+				<p>Où nous trouver:</p>
+				<p>
+					140 Route 9 rang <br>
+					Ste-Séraphine <br> 
+					QC J0A 1E0
+				</p>
+				<p>
+					819-336-5649
+				</p>
+			</div>
+		</div>
 	</div>
 
 	<!-- google map -->
