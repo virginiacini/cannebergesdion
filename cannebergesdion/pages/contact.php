44c48004f2bf3bc7cab95e4957158fef5b509151
virginiacini <virginiacini@gmail.com>
2015-06-23 15:42:56 -0400

    last commit

--- cannebergesdion/pages/contact.php
+++ cannebergesdion/pages/contact.php
@@ -6,6 +6,39 @@
 	  	<hr>	
 	  </div>
 	</div><!-- end row -->
+	
+	<div class="row">
+		<form method='post' action='contactform.php' name='contactform' id='contactform'>
+			<p>
+			<label for='fullname'>Your Name:</label><br/>
+			<input type='text' name='fullname' />
+			</p>
+
+			<p>
+			<label for='email' >Email Address:</label><br/>
+			<input type='text' name='email' />
+			</p>
+			<input type='submit' name='submit' value='Submit Form' />
+		</form>
+		<script>
+		document.contactform.onsubmit=function()
+		{
+			if(document.contactform.fullname.value =='')
+			{
+				alert("Please enter your name");
+				document.contactform.fullname.focus();
+				return false;
+			}
+			else if(document.contactform.email.value =='')
+			{
+				alert("Please enter your Email address");
+				document.contactform.email.focus();
+				return false;
+			}
+			return true;
+		}
+		</script>
+	</div>
 
 	<!-- google map -->
 	<div class="row">
